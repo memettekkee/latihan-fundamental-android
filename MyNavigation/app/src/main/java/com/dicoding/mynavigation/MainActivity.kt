bdf474b6826752012d10380e100f3f59bfc1602c
Muhammad <muhammadahjahh@gmail.com>
2024-03-14 14:07:32 +0700

    bind

---
+++ MyNavigation/app/src/main/java/com/dicoding/mynavigation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.dicoding.mynavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.mynavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
