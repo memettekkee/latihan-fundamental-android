437234cc2c731dcb705e3a392519f708811c67af
Muhammad <muhammadahjahh@gmail.com>
2024-03-14 21:46:11 +0700

    pop up behavior (kalo dipencet button balik ke fragment semula)

--- MyNavigation/app/src/main/java/com/dicoding/mynavigation/DetailCategoryFragment.kt
+++ MyNavigation/app/src/main/java/com/dicoding/mynavigation/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.dicoding.mynavigation.databinding.FragmentDetailCategoryBinding
 
 
@@ -38,6 +39,10 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Sisa : $dataDescription"
+
+        binding.btnHome.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
     override fun onDestroy() {
