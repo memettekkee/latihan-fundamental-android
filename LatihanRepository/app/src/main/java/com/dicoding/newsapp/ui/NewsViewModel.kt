7b83c932857faeeb03d7fe15570129d0b85cb2e3
Muhammad <muhammadahjahh@gmail.com>
2024-03-27 15:01:27 +0700

    tambah fitur bookmarked

--- LatihanRepository/app/src/main/java/com/dicoding/newsapp/ui/NewsViewModel.kt
+++ LatihanRepository/app/src/main/java/com/dicoding/newsapp/ui/NewsViewModel.kt
@@ -1,8 +1,19 @@
 package com.dicoding.newsapp.ui
 
 import androidx.lifecycle.ViewModel
+import com.dicoding.newsapp.data.local.entity.NewsEntity
 import com.dicoding.newsapp.data.source.NewsRepository
 
 class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
     fun getHeadlineNews() = newsRepository.getHeadlineNews()
+
+    fun getBookmarkedNews() = newsRepository.getBookmarkedNews()
+
+    fun saveNews(news: NewsEntity) {
+        newsRepository.setBookmarkedNews(news, true)
+    }
+
+    fun deleteNews(news: NewsEntity) {
+        newsRepository.setBookmarkedNews(news, false)
+    }
 }
